23df42ca57ecf14f3df25045b25c948d2b509679
Pour1a-D <pour1a.dra80@gmail.com>
2024-06-05 21:37:25 +0330

    added mongo DB connection method

---
+++ lib/db.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+export default async function connect() {
+    const connectionState = mongoose.connection.readyState;
+
+    if (connectionState === 1) {
+        console.log("Already connected!");
+        return;
+    }
+
+    if (connectionState === 2) {
+        console.log("Connecting ...");
+        return;
+    }
+
+    try {
+        mongoose.connect(MONGODB_URI!, {
+            dbName: "REST-API-Next-14",
+            bufferCommands: true,
+        });
+
+        console.log("Connected");
+    } catch (error: any) {
+        console.log("Error :", error);
+        throw new Error("Error :", error);
+    }
+}
